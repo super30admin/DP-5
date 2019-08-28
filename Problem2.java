e9b2038825979c10532e34719c8e943a9e09d45f
bharatreddypothireddy <35764299+bharatreddypothireddy@users.noreply.github.com>
2019-08-27 19:10:59 -0500

    Completed DP-5 - Problem 2

---
+++ Problem2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if( m == 0 || n == 0)   return 0;
+        if(m == 1 || n == 1)    return 1;
+        
+        int[][] dp = new int[m][n];
+        for(int i = 0;i<dp.length;i++)
+            dp[i][0] = 1;
+        for(int j = 0;j<dp[0].length;j++)
+            dp[0][j] = 1;
+        for(int i = 1;i<dp.length;i++){
+            for(int j = 1;j<dp[0].length;j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
