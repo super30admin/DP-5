309be7431a6b2549fbff82d97776e90d5ac62511
Lavan Kumar <11154769+lavanganji@users.noreply.github.com>
2019-08-30 18:53:33 -0500

    updated the class solution using brute force

--- Problem1.java
+++ Problem1.java
@@ -24,3 +24,25 @@ class Solution {
     }
 
 }
+// Class Soltuions
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        return helper(s,set);
+    }
+    
+    public boolean helper(String s, HashSet<String> set){
+        //base
+        if(s.length() == 0) return true;
+    
+        for(int i=0;i<=s.length();i++){
+            if(set.contains(s.substring(0,i)) &&
+              helper(s.substring(i),set)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
