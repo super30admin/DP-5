c9959473f7b5669767f2f9a13e6dacf0efaf9fb8
bharatreddypothireddy <35764299+bharatreddypothireddy@users.noreply.github.com>
2019-08-30 12:21:32 -0500

    Completed DP - 5 - Problem 2
    Added recursive solution as well as iterative.

--- Problem2.java
+++ Problem2.java
@@ -26,4 +26,11 @@ class Solution {
         }
         return dp[m-1][n-1];
     }
+    
+    // recursive solution
+    public int uniquePaths2(int m, int n) {
+        if( m == 0 || n == 0)   return 0;
+        if(m == 1 || n == 1)    return 1;
+        return (uniquePaths(m-1,n)+uniquePaths(m,n-1));
+    }
 }
