590d89fbc0b7cfe35b0c553a9cb3572372647891
RamyaRaviprakash <rramya@thoughtworks.com>
2019-10-15 12:49:44 -0700

    word break

--- Sample.java
+++ Sample.java
@@ -23,3 +23,24 @@ class Solution {
         return dp[m-1][n-1];
     }
 }
+
+// Time complexity: O(s.length * s.length) in worst case
+// Space complexity: O(s.length)
+
+// Approach: To check if a word is breakable, 
+// check if its substring of say (0,i) is breakable and if substring from i+1 to end of word is in dictionary.
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for(int i=0;i<s.length();i++){
+            for(int j=i-1;j>=-1;j--){
+                if(dp[j+1] && wordDict.contains(s.substring(j+1,i+1))){
+                    dp[i+1] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
