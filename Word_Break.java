737c249e3f64109c8ff32036da01de8deae029c4
Karthik Bangera <adikar08@gmail.com>
2020-05-13 19:07:32 -0400

    Completed word break question

---
+++ Word_Break.java
@@ -0,0 +1,24 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for(int i = 0; i<dp.length; i++){
+            for(int j = 0; j<i; j++){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i]=true;
+                }
+            }
+        }
+        
+        return dp[dp.length-1];
+    }
+}
