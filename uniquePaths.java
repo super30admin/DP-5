7278976cc68c2a92f04233c8b6a69cae8604f0f0
albertcoder <eralbert9191@gmail.com>
2020-08-14 15:00:53 -0700

    both recursive and DP solutions added

--- uniquePaths.java
+++ uniquePaths.java
@@ -1,3 +1,4 @@
+//APPROACH 1: recursive
 //let m and n be the rows and columns of the matrix
 //time complexity O(2^(mn))
 //space complexity O(1)
@@ -17,5 +18,23 @@ class Solution {
         //System.out.println("bottom" +  (i + 1) + "," + j);
         int bottom = helper(i+1, j, m, n);
         return right + bottom;
+    }
+}
+
+//APPROACH 2: DP
+//time complexity O(m x n)
+//space complexity O(1)
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        dp[m-1][n-1] = 1;
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                if(i == m -1 && j == n-1) continue;
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 }
