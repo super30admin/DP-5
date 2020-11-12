455af98cd34c53017888ef16e6aef5524ace3e86
SowmyaJupally <63617870+SowmyaJupally@users.noreply.github.com>
2020-11-11 18:27:48 -0800

    DP-5 Done

---
+++ wordBreak.py
@@ -0,0 +1,19 @@
+#Time Complexity: O(npowern)
+#Space Complexity: O(N)
+#Run on Leetcode: Yes
+#Any Issues: No
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+         N = len(s)
+    dp = [False]*N
+    
+    for i in range(N):
+        if s[:i+1] in wordDict: 
+            dp[i] = True
+        else:
+           for j in range(i):
+               if dp[j] == True and s[j+1:i+1] in wordDict:
+                   dp[i] = True
+    
+    return dp[-1]
