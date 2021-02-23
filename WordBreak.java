e42645a1e584d05d6dbbc2e453aa85625830fa47
Nilesh Deokar <76528900+nilesh-525@users.noreply.github.com>
2021-02-23 14:21:18 -0500

    Update WordBreak.java

--- WordBreak.java
+++ WordBreak.java
@@ -23,4 +23,32 @@ class WordBreak {
         
         return false;
     }
+    
+    /*
+        Time : O(N)
+        Space : O(M + N) | M = size of wordDict, N length of a string.
+        Leetcode : YES
+    */
+    
+    /*
+        Approach : DP
+        create a DP array and iterate over it, at every stage check if 0 to i-1 exists in dict or not.
+    */
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if(s == null || s.length() == 0) return false;
+        
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean [] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 0; j < i ; j++){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i] = true; 
+                    break;
+                }
+            }
+        }
+                   
+        return dp[dp.length - 1];
+    }
 }
