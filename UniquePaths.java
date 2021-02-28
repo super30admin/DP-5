b85b16a1f7266e2681433760820a22561097c4c7
unknown <eo8935@gmail.com>
2021-02-28 13:32:23 -0500

    initial commit

---
+++ UniquePaths.java
@@ -0,0 +1,33 @@
+// Time Complexity :O(n*m)
+// Space Complexity : O(n*m)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : getting started
+
+// Your code here along with comments explaining your approach
+
+public class UniquePaths {
+
+    class Solution {
+        public int uniquePaths(int m, int n) {
+
+            //create 2d dp array and fille first row and cols with 1
+            int[][] dp = new int[m][n];
+
+            for (int i = 0; i < dp.length; i++) {
+                dp[i][0] = 1;
+            }
+            for (int i = 0; i < dp[0].length; i++) {
+                dp[0][i] = 1;
+            }
+
+            //perfom dp
+            for (int i = 1; i < dp.length; i++) {
+                for (int j = 1; j < dp[i].length; j++) {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+            return dp[m-1][n-1];
+        }
+    }
+
+}
