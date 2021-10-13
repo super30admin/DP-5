96ec66380ce41ae299e184a51d0dd13380a3823b
balasudip <balasudip6@gmail.com>
2021-10-13 13:30:23 -0700

    Completed Sprint DP-5

---
+++ Problem111_unique-paths.java
@@ -0,0 +1,14 @@
+// Time Complexity: O(m*n)
+// Space Complexity: O(n)
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for(int i = m - 2; i >= 0; i--) {
+            for(int j = n - 2; j >= 0; j--) {
+                dp[j] = dp[j + 1] + dp[j];  
+            }
+        }
+        return dp[0];
+    }
+}
