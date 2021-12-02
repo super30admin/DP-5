5f8bf0c73139cc15282b8f249bb8877f9cedfba8
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-12-01 19:52:23 -0500

    Update uniquepath.java

--- uniquepath.java
+++ uniquepath.java
@@ -1,5 +1,5 @@
 //Time complexity: o(mn)
-//space: o(1)
+//space: o(mn)
 
 class Solution {
     public int uniquePaths(int m, int n) {
@@ -15,3 +15,27 @@ class Solution {
         return dp[0][0];
     }
 }
+
+//Optimal Solution
+
+//TC: O(m*n)
+//SC: O(n)
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m < n) return uniquePaths(n, m);
+        
+        int [] dp = new int[n];
+        Arrays.fill(dp, 1);
+        
+        for(int i = m-2; i>=0; i--){
+            int right = 0;
+            for(int j = n-1; j>=0; j--){
+                dp[j] = dp[j] + right;
+                right = dp[j];
+            }
+        }
+        return dp[0];
+    }
+}
+ 
