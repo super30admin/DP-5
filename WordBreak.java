66747571085309133d2074af97f0baaaa58515dc
harshaanirudh <harsha.anirudh@gmail.com>
2022-02-18 15:56:26 -0800

    Update WordBreak.java

--- WordBreak.java
+++ WordBreak.java
@@ -23,3 +23,22 @@ class Solution {
         return false;
     }
 }
+//Time Complexity:O(n3)
+//Space Complexity:O(n)
+class Solution {
+    HashSet<String> set;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        set= new HashSet<>(wordDict);
+        boolean dp[]=new boolean[s.length()+1];
+        dp[0]=true;
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<=i;j++){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
