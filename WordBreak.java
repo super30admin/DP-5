88b764460f5bd0c520f062ec917ff4e2a8c08e8c
Hitesh Purshotam Dayaramani <hdayaram@tibco.com>
2022-04-26 12:21:21 +0530

    Completed DP-5

---
+++ WordBreak.java
@@ -0,0 +1,23 @@
+//Time Complexity O(M^2)
+//Space Complexity O(N)
+//Leetcode tested
+
+import java.util.HashSet;
+import java.util.List;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
