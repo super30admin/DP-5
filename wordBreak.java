7dfe456915c1360f08c3e5ec1c4bd1ef434fa146
Hanuman Reddy <39452135+HanumanReddy1212@users.noreply.github.com>
2022-07-19 20:08:27 -0700

    DP-5 done

---
+++ wordBreak.java
@@ -0,0 +1,28 @@
+// Time Complexity :n^4 if we consider that set will take O(n) to check contains
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :yes
+
+class Solution {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>();
+        int strLen = s.length();
+        boolean[] result = new boolean[strLen + 1];
+        for (String word : wordDict) {
+            set.add(word);
+        }
+        result[0] = true;
+        for (int i = 1; i < result.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (result[j]) {
+                    String sub = s.substring(j, i);
+                    if (set.contains(sub)) {
+                        result[i] = true;
+                    }
+                }
+            }
+        }
+        return result[strLen];
+    }
+
+} 
