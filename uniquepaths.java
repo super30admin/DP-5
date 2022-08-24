cb69492ef3ec717e5c6a2fbc6e04ca4d5f719cb2
Shubham <shub01gupta@gmail.com>
2022-08-24 10:51:20 +0530

    Completed DP-5

---
+++ uniquepaths.java
@@ -0,0 +1,18 @@
+// TC : O(m*n)
+// SC : O(n)
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m==0 || n==0) return 0;
+        
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[j] = dp[j+1] + dp[j];
+            }
+        }
+        return dp[0];
+    }
+}
