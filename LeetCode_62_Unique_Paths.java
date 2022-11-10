2a02b433fcc3a64a9609ef2c1a3e692357ed08c3
khushbukareliya07 <khushbu.karelia@gmail.com>
2022-11-10 17:57:28 -0600

    Leetcode 62 - Unique path - OPTIMIZED DP approach added

--- LeetCode_62_Unique_Paths.java
+++ LeetCode_62_Unique_Paths.java
@@ -38,3 +38,41 @@ class Solution {
         return rightPath + bottomPath;
     }
 }
+
+
+//DP approach
+/*
+1. we will take a matrix of siex m+1, n+1
+2. by default all the fields intialized to 0 in java
+3. we amke sure to update the m-1, n-1 position as 1 as that's th destination we want to reach to. 
+4. at one, we start running a for loop backward, from m-1 to m and n-1 to n
+while ding this, partciualr cell will hold the sum == right cell + bottom cell
+so, for any cell x, it's value  = right side + bottom side cell/ 
+and this way we will keep filling our all the cells until we reach the first cell. 
+5. at last our answer will lie in m=0 and n=0
+TC - O(m*n) - raversing through and building matrix. 
+sc - O(m*n)
+*/
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m ==0|| n==0) return 0;
+        
+        //for ur dp matrix
+        m = m+1; 
+        n = n+1;
+        
+        int[][] dp = new int[m][n];
+        dp[m-2][n-2] = 1;
+        
+        for(int i = m-2; i>=0; i--)
+        {
+            for(int j = n-2; j>=0; j--)
+            {
+                if(i == m-2 && j == n-2) continue; // otherwise loop will update this value as well 
+                dp[i][j] =  dp[i][j+1] + dp[i+1][j];
+            }
+        }
+        return dp[0][0];
+    }
+}
