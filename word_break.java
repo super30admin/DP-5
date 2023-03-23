0eb6742fe2b70fe159e56c1889d2a614e37f74de
MalavyaRaval <malavyaraval@gmail.com>
2023-03-22 17:23:01 -0700

    Completed

---
+++ word_break.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean [] dp = new boolean[s.length() +1];
+        HashSet<String> set = new HashSet<>(wordDict);
+        dp[0] = true;
+        for(int i = 0;i<dp.length;i++){
+            for(int j= 0;j<i;j++){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i] =true;
+                }
+            }
+        }
+    return dp[dp.length-1];
+    }
+}
