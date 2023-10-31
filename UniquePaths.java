2cd6d9f62be54fa3449215a3ad82eac6a411c6eb
UmangThakur15 <umangthakuremail@gmail.com>
2023-10-30 22:24:33 -0400

    Unique Paths DP approach

---
+++ UniquePaths.java
@@ -0,0 +1,27 @@
+//TC will be O(m * n)
+//SC will be (m * n)
+
+
+class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        if(m == 0 && n == 0){
+            return 0;
+        }
+
+        int[][] dp = new int[m+1][n+1];
+        dp[m][n-1] = 1;
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                dp[i][j] = dp[i][j + 1] + dp[i + 1][j];
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static void main(String[] args){
+        UniquePaths obj = new UniquePaths();
+        int m = 3;
+        int n = 7;
+        System.out.println(obj.uniquePaths(m,n));
+    }
+}
