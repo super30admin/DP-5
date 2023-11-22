544a45b0a6e2aea31bd04cac41f45d6e881df828
Rucha Tambe <ruchatambe@Ruchas-MacBook-Air.local>
2023-11-21 20:00:03 -0800

    Complete DP-5

---
+++ Solution.java
@@ -0,0 +1,27 @@
+
+// Time Complexity : O(n^3)
+// Space Complexity :  O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        int n = s.length();
+        for(int i =1; i<=n;i++){
+            for(int j = 0;j<i;j++){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+        
+    }
+}
