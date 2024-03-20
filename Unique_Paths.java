2d9fb9e5d2e71e8b599635e963be3e9cde47347c
SINDHURA_PARUCHURI <paruchuri.sindhu13@gmail.com>
2024-03-19 21:17:36 -0700

    Completed dp-5

---
+++ Unique_Paths.java
@@ -0,0 +1,19 @@
+//TimeComplexity : m*n
+//Space Complexity: m*n
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int [][]dp = new int[m+1][n+1];
+        dp[m-1][n-1] = 1;
+        for(int i = m-1; i>=0;i--) {
+            for(int j =n-1;j>=0;j--) {
+                if(i == m-1 || j == n-1) {
+                    dp[i][j] = 1;
+            } else {
+                dp[i][j] = dp[i][j+1] + dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
